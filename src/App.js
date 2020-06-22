6111480bbd092771e8156c921d8e6555f970ae46
Salman Azeem <salmanazeemkhan@gmail.com>
2020-06-22 12:19:47 +0400

    Files and Component Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { AddTransaction, Balance, Header, IncomeExpenses, Transaction, TransactionList } from './components/index.js';
+
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
+      <Header />
+      <IncomeExpenses />
+      <TransactionList />
+      <Balance />
+      <AddTransaction />
+      <Transaction />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import AddTransaction from './AddTransaction';
+import Balance from './Balance';
+import Header from './Header';
+import IncomeExpenses from './IncomeExpenses';
+import Transaction  from'./Transaction';
+import TransactionList from './TransactionList'; 
+
+export {
+    AddTransaction,
+    Balance,
+    Header,
+    IncomeExpenses,
+    Transaction,
+    TransactionList
+}
+
+export default {
+    AddTransaction: AddTransaction,
+    Balance: Balance,
+    Header: Header,
+    IncomeExpenses: IncomeExpenses,
+    Transaction: Transaction,
+    TransactionList: TransactionList
+}
